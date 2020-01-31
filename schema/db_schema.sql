82ea2ca01961ded252a6699c2249d8a338d29aad
Jeremy Chan <chanjem@gmail.com>
2020-01-31 19:08:34 +0800

    Update db schema on naming convention

--- schema/db_schema.sql
+++ schema/db_schema.sql
@@ -8,20 +8,20 @@ DROP TABLE IF EXISTS `students`;
 
 CREATE TABLE `teachers` (
     id int NOT NULL AUTO_INCREMENT,
-    teacher_email varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE `students` (
     id int NOT NULL AUTO_INCREMENT,
-    student_email varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
     is_suspended BOOLEAN NOT NULL DEFAULT 0,
     PRIMARY KEY(id)
 );
 
 CREATE TABLE `notifications` (
-    teacher_email int NOT NULL,
-    student_email int NOT NULL,
-    FOREIGN KEY(teacher_email) REFERENCES teachers(id),
-    FOREIGN KEY(student_email) REFERENCES students(id)
+    teacher_email_id int NOT NULL,
+    student_email_id int NOT NULL,
+    FOREIGN KEY(email) REFERENCES teachers(id),
+    FOREIGN KEY(email) REFERENCES students(id)
 );
